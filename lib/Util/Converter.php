daa102c127bd20c9dfa46a592c7275fbbefc622c
PaulGirardin <paul@shoprunback.com>
2018-03-01 10:37:29 +0100

    Refactor and comment Converter

--- lib/Util/Converter.php
+++ lib/Util/Converter.php
@@ -11,57 +11,56 @@ abstract class Converter
         $object = new $targetObject();
 
         foreach ($baseObject as $key => $value) {
-            $keyAsClassName = ucfirst($key);
-            $trimedKeyClassName = rtrim($keyAsClassName, 's');
-
-            if (class_exists(self::RESSOURCES_NAMESPACE . $trimedKeyClassName) && ! class_exists(self::RESSOURCES_NAMESPACE . $keyAsClassName)) {
-                $valueToAdd = [];
-
-                foreach ($value as $k => $v) {
-                    $valueToAdd[] = self::convertToSRBObject($v, self::RESSOURCES_NAMESPACE . $trimedKeyClassName);
-                }
-
-                $object->$key = $valueToAdd;
-            } else {
-                $object->$key = class_exists(self::RESSOURCES_NAMESPACE . $keyAsClassName) ? self::convertToSRBObject($value, self::RESSOURCES_NAMESPACE . $keyAsClassName) : self::searchSRBObjectsInArrayOrObject($value);
-            }
+            $object->$key = self::getContent($key, $value);
         }
 
         return $object;
     }
 
-    private static function searchSRBObjectsInArrayOrObject($container)
+    public static function searchSRBObjectsInArrayOrObject($container)
     {
+        // If it is neither an array nor an object, we simply return the value
         if (! is_array($container) && ! is_object($container)) {
             return $container;
         }
 
+        // We create the appropriate var
         $checked = ! is_array($container) ? new \StdClass() : [];
 
         foreach ($container as $key => $value) {
-            $keyAsClassName = ucfirst($key);
-            $trimedKeyClassName = rtrim($keyAsClassName, 's');
-            $valueToAdd = $value;
-
-            if (class_exists(self::RESSOURCES_NAMESPACE . $keyAsClassName)) { // Check if value is a resource
-                $valueToAdd = self::convertToSRBObject($value, self::RESSOURCES_NAMESPACE . $keyAsClassName);
-            } else if (class_exists(self::RESSOURCES_NAMESPACE . $trimedKeyClassName)) { // Check if value is an array of resources
-                $valueToAdd = [];
-
-                foreach ($value as $k => $v) {
-                    $valueToAdd[] = self::convertToSRBObject($v, self::RESSOURCES_NAMESPACE . $trimedKeyClassName);
-                }
-            } else if (is_array($value) || is_object($value)) { // Check if value is an array or an object
-                $valueToAdd = self::searchSRBObjectsInArrayOrObject($value);
-            }
-
+            // We add the value to the container with the according syntax
             if (is_array($checked)) {
-                $checked[$key] = $valueToAdd;
+                $checked[$key] = self::getContent($key, $value);
             } else {
-                $checked->$key = $valueToAdd;
+                $checked->$key = self::getContent($key, $value);
             }
         }
 
         return $checked;
+    }
+
+    private static function getContent($key, $value)
+    {
+        // We get the key that may be a class name
+        $keyAsClassName = ucfirst($key);
+        // We check if the object is an array of objects we have a class for (ex: items)
+        $trimedKeyClassName = rtrim($keyAsClassName, 's');
+        // The value we will return
+        $valueToAdd;
+
+        // If the trimed string is a class but not the not-trimed, it means it is an array of objects we have a class for
+        if (class_exists(self::RESSOURCES_NAMESPACE . $trimedKeyClassName) && ! class_exists(self::RESSOURCES_NAMESPACE . $keyAsClassName)) {
+            $valueToAdd = [];
+
+            // Since it is an array only of objects we have a class for, we call the recursive function to convert each object
+            foreach ($value as $k => $v) {
+                $valueToAdd[] = self::convertToSRBObject($v, self::RESSOURCES_NAMESPACE . $trimedKeyClassName);
+            }
+        } else {
+            // If we have a class for the object, we convert it. Or else, we go to the normal recursive function
+            $valueToAdd = class_exists(self::RESSOURCES_NAMESPACE . $keyAsClassName) ? self::convertToSRBObject($value, self::RESSOURCES_NAMESPACE . $keyAsClassName) : self::searchSRBObjectsInArrayOrObject($value);
+        }
+
+        return $valueToAdd;
     }
 }
