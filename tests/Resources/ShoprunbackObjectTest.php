141e8da9b738ca3d8159b785fad36e410ab6b82b
Julien Malherbe <julien@malherbe.net>
2018-03-08 15:43:24 +0100

    add missing file

---
+++ tests/Resources/ShoprunbackObjectTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Resources;
+
+use \Tests\BaseTest;
+
+use \Shoprunback\Resources\ShoprunbackObject;
+use \Shoprunback\Resources\Brand as Brand;
+use \Shoprunback\Util\Inflector;
+use \Shoprunback\RestClient;
+
+abstract class ShoprunbackObjectTest extends BaseTest
+{
+    static public function getResourcePath()
+    {
+        return Inflector::RESSOURCES_NAMESPACE . static::getClass();
+    }
+
+    // public function testCanFetchWithId()
+    // {
+    //     RestClient::getClient()->enableTesting();
+
+    //     $object = static::getResourcePath()::retrieve('thenamedoesnotmatter');
+    //     $this->assertInstanceOf(
+    //         static::getResourcePath(),
+    //         $object
+    //     );
+    // }
+
+    // public function testCanCreate()
+    // {
+    //     $item = static::getObjectSample();
+    //     $this->assertNull($item->save());
+    // }
+
+    // public function testCanUpdate()
+    // {
+    //     $item = static::getResourcePath()::fetch(static::getObjectToFetch());
+    //     $item = static::updateForTest($item);
+    //     $this->assertNull($item->save());
+    // }
+}
