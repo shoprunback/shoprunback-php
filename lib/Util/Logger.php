0d8ae5df5ade11f371c581e89ec2cebe014d81fd
PaulGirardin <paul@shoprunback.com>
2018-03-12 11:56:10 +0100

    Dix single tests in CLI and add tests for Logger

--- lib/Util/Logger.php
+++ lib/Util/Logger.php
@@ -6,12 +6,16 @@ define('LOG_PATH', dirname(__FILE__, 3) . '/logs/');
 
 class Logger
 {
+    const FILENAME_DATE_FORMAT = 'Y-m-d';
+    const LINE_PREFIX_DATE_FORMAT = '[Y-m-d H:i:s]: ';
+    const FILE_EXTENSION = '.txt';
+
     const INFO = 0;
     const ERROR = 1;
 
     static private function log($message = '', $logType = self::INFO)
     {
-        $filePath = LOG_PATH . date('Y-m-d') . '.txt';
+        $filePath = LOG_PATH . date(self::FILENAME_DATE_FORMAT) . self::FILE_EXTENSION;
 
         // Create file if doesn't exist
         if (!file_exists($filePath)) {
@@ -19,7 +23,7 @@ class Logger
             chmod($filePath, 0777);
         }
 
-        error_log(date('[Y-m-d H:i:s]: ') . $message . "\n", 3, $filePath);
+        error_log(date(self::LINE_PREFIX_DATE_FORMAT) . $message . "\n", 3, $filePath);
     }
 
     static public function info($message = '')
@@ -30,5 +34,12 @@ class Logger
     static public function error($message = '')
     {
         self::log($message, self::ERROR);
+    }
+
+    static public function getLastLine($dateToFormat = '')
+    {
+        $date = $dateToFormat ? date($dateToFormat, self::FILENAME_DATE_FORMAT) : date(self::FILENAME_DATE_FORMAT);
+        $allLines = explode("\n", file_get_contents(LOG_PATH . $date . self::FILE_EXTENSION));
+        return $allLines[count($allLines) - 2];
     }
 }
--- tests/Util/LoggerTest.php
+++ tests/Util/LoggerTest.php
@@ -7,15 +7,34 @@ namespace Tests\Resources;
 use \Tests\BaseTest;
 
 use \Shoprunback\RestClient;
+use \Shoprunback\Resources\Brand;
+use \Shoprunback\Util\Logger;
 
-final class BrandTest extends BaseTest
+define('LOG_FILENAME', date('Y-m-d') . '.txt');
+define('TESTS_LOG_PATH', dirname(__FILE__, 3) . '/logs/');
+
+final class LoggerTest extends BaseTest
 {
-    const CLASS_NAME = 'Shoprunback\Resources\Brand';
+    const CLASS_NAME = 'Shoprunback\Util\Logger';
+    const PATH_TO_LOG_FILE = TESTS_LOG_PATH . LOG_FILENAME;
 
-    public function testCanFetchOneMocked()
+    public function testCanCreateFile()
+    {
+        Logger::info('Test');
+        $this->assertTrue(file_exists(self::PATH_TO_LOG_FILE));
+    }
+
+    public function testCanLogError()
+    {
+        Logger::error('Error');
+        $this->assertTrue(strpos(Logger::getLastLine(), 'Error') > 0);
+    }
+
+    public function testCanLogResource()
     {
         RestClient::getClient()->enableTesting();
+        Brand::delete(1);
 
-        $this->checkIfHasNeededValues($brand);
+        $this->assertTrue(strpos(Logger::getLastLine(), date(Logger::LINE_PREFIX_DATE_FORMAT) . 'Brand:') === 0);
     }
 }
