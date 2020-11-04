c31a148e52bedd8ed2c29a5693c0bc1cc1bc20a1
Choub Chanthou <chanthou_choub@hotmail.com>
2020-11-04 10:08:05 +0700

    unit testing: Add spare part test api

---
+++ tests/Elements/Api/SparePartTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Elements\Api;
+
+use \Tests\Elements\Api\BaseApiTest;
+
+use \Shoprunback\Elements\SparePart;
+use \Shoprunback\RestClient;
+use \Shoprunback\Error\NotFoundError;
+
+final class SparePartTest extends BaseApiTest
+{
+    use \Tests\Elements\SparePartTrait;
+
+    public function testCanSaveNewSparePart()
+    {
+        static::disableTesting();
+
+        $sparePart = self::createDefault();
+
+        $name = $sparePart->name;
+        $reference = $sparePart->reference;
+
+        $sparePart->save();
+
+        $this->assertNotNull($sparePart->id);
+        $this->assertSame($sparePart->name, $name);
+        $this->assertSame($sparePart->reference, $reference);
+    }
+
+    public function testCanUpdate()
+    {
+        static::disableTesting();
+
+        $sparePart = SparePart::retrieve(1);
+        $sparePartId = $sparePart->id;
+        $name = self::randomString();
+        $sparePart->name = $name;
+        $sparePart->picture_url = "http://1001startups.fr/wp-content/uploads/2016/05/SRB-Logo-primaire-couleurs.jpg";
+        $sparePart->save();
+
+        $retrievedSparePart = SparePart::retrieve($sparePartId);
+
+        $this->assertSame($retrievedSparePart->name, $name);
+    }
+}
